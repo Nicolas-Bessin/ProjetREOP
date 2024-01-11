c89d896d527d978681d0de5d76d0908a8aff5318
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-01-11 18:06:38 +0100

    OnlyFurthestSites

--- src/agregator.jl
+++ src/agregator.jl
@@ -4,8 +4,6 @@
 include("parser.jl")
 include("instance.jl")
 
-sizes = ["tiny", "small", "medium", "large", "huge"]
-
 function meanPowerScenario(instance :: Instance)
     meanPower = sum([w.power * w.probability for w in instance.windScenarios])
     meanScenario = WindScenario(1, meanPower, 1.0)
@@ -61,7 +59,32 @@ function quartersPowerScenario(instance :: Instance)
     )
 end
 
+function onlyFurthestSites(instance :: Instance, nbCoordinates :: Int64 = 2)
+    # We only keep the substation sites closest to the turbines
+    # All the instances have the turbines in X > 0, furthest away from the main substation (in X = 0)
+    Xpos = unique([site.x for site in instance.substationLocations])
+    sort!(Xpos, rev = true)
+    locations = [site for site in instance.substationLocations if site.x in Xpos[1:nbCoordinates]]
+    return Instance(
+        instance.curtailingCost,
+        instance.curtailingPenalty,
+        instance.maxCurtailment,
+        instance.fixedCostCable,
+        instance.variableCostCable,
+        instance.mainLandSubstation,
+        instance.maximumPower,
+        instance.landSubstationCables,
+        instance.substationSubstationCables,
+        locations,
+        instance.substationTypes,
+        instance.windScenarios,
+        instance.windTurbine,
+    )
+end
+
+
 function aggregateInstances(outputFormat :: String, aggregationFunction :: Function)
+    sizes = ["small", "medium", "large", "huge"]
     for size in sizes
         inputFile = "instances/KIRO-$size.json"
         originalInstance = read_instance(inputFile)
@@ -74,4 +97,5 @@ function idem(instance :: Instance)
     return instance
 end
 
-aggregateInstances("instances/aggregated/KIRO-quarters", quartersPowerScenario)
+aggregateInstances("instances/aggregated/onlyFurthestSites", onlyFurthestSites)
+
