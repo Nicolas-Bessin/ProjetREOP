30cef4917ed511d1101569673ff381076c95cd3b
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-01-13 18:32:37 +0100

    large not working

--- src/main.jl
+++ src/main.jl
@@ -5,14 +5,14 @@ include("costCompute.jl")
 include("agregator.jl")
 
 #PARMETERS TO CHANGE#
-# Example : for small, onlyFurthestSites+ninetyFivePercentWorse agragations, use :
+# Example : for small, onlyFurthestSites+ninetyFivePercentWorse agregations, use :
 # size = "small"
 # aggregationMethod = "onlyFurthestSites+ninetyFivePercentWorse"
 #####################
 # To use the original instance && compute the full MILP, use :
 # aggregationMethod = ""
-size = "tiny"
-aggregationMethod = ""
+size = "large"
+aggregationMethod = "onlyFurthestSites+ninetyFivePercentWorse"
 #####################
 
 trueInstanceFile = "instances/KIRO-$size.json"
@@ -26,12 +26,12 @@ end
 trueInstance = read_instance(trueInstanceFile)
 
 # CHANGE THIS LINE TO CHANGE THE AGGREGATION METHOD #
-# Example : for small, onlyFurthestSites+ninetyFivePercentWorse agragations, use :
+# Example : for small, onlyFurthestSites+ninetyFivePercentWorse agregations, use :
 # instance = xPercentWorseScenario(onlyFurthestSites(trueInstance), 0.95)
 #####################################################
 # To use the original instance && compute the full MILP, use :
 # instance = trueInstance
-instance = trueInstance
+instance = xPercentWorseScenario(onlyFurthestSites(trueInstance), 0.95)
 #####################################################
 
 if aggregationMethod != ""
@@ -41,12 +41,12 @@ end
 solution, time = linearSolver(instance)
 
 # CHANGE THIS LINE IF USING A METHOD THAT REQUIRES DE-AGGREGATION #
-# Example : for small, onlyFurthestSites+ninetyFivePercentWorse agragations, use :
+# Example : for small, onlyFurthestSites+ninetyFivePercentWorse agregations, use :
 # trueSolution = deAggregateReducedSiteSolution(trueInstance, instance, solution)
 ###################################################################
 # To use the original instance && compute the full MILP, use :
 # trueSolution = solution
-trueSolution = solution
+trueSolution = deAggregateReducedSiteSolution(trueInstance, instance, solution)
 ###################################################################
 
 writeSolution(trueSolution, "solutions/$outputFormat.json")
